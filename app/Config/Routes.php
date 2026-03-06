3e30202ced9057c2c3d74d35877022ae52e92340
Philip Newborough <corenominal@corenominal.org>
2026-03-06 08:35:51 +0000

    New routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// API routes
+$routes->match(['get', 'options'], '/api/test/ping', 'Api\Test::ping');
+$routes->match(['post', 'options'], '/api/message', 'Api\Message::index');
+
+// Command line routes
+$routes->cli('cli/test/index/(:segment)', 'CLI\Test::index/$1');
+$routes->cli('cli/test/count', 'CLI\Test::count');
+$routes->cli('cli/sendmail/process', 'CLI\Sendmail::process');
