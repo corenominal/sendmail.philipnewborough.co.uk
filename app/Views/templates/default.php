d30cebca0a0a9c567580a5909620179373b74a1d
Philip Newborough <corenominal@corenominal.org>
2026-03-09 20:11:40 +0000

    Change main navbar title

--- app/Views/templates/default.php
+++ app/Views/templates/default.php
@@ -45,7 +45,7 @@
             <div class="container-fluid px-0">
                 <a class="navbar-brand d-flex align-items-center gap-2 ms-3" href="<?= site_url() ?>">
                     <img src="/icon.svg" alt="Logo" width="45" height="45" class="d-inline-block align-text-top rounded-circle my-1">
-                    Philip Newborough
+                    Sendmail Service
                 </a>
 
                 <div>
